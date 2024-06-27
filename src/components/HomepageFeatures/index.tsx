b92333fdfd39d9ffa0aecb25b9cfadd8051bef6a
Vladyslav Palyvoda <vladyslav_palyvoda@epam.com>
2024-06-27 17:46:25 +0300

    docs: Update pricing and home page

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,114 +1,137 @@
-import clsx from 'clsx';
-import Heading from '@theme/Heading';
-import styles from './styles.module.css';
+import clsx from "clsx";
+import Heading from "@theme/Heading";
+import styles from "./styles.module.css";
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  Svg: React.ComponentType<React.ComponentProps<"svg">>;
   description: JSX.Element;
   link: string;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Application scaffolding',
-    Svg: require('@site/static/img/logo.svg').default,
+    title: "Application scaffolding",
+    Svg: require("@site/static/img/logo.svg").default,
     description: (
       <>
-        Accelerate your delivery process with our versatile Application templates. Expand your possibilities by extending our comprehensive library with your personalized templates.
+        Accelerate your delivery process with our versatile Application
+        templates. Expand your possibilities by extending our comprehensive
+        library with your personalized templates.
       </>
     ),
     link: "docs/user-guide/add-application/",
   },
   {
-    title: 'CI/CD pipelines',
-    Svg: require('@site/static/img/logo.svg').default,
+    title: "CI/CD pipelines",
+    Svg: require("@site/static/img/logo.svg").default,
     description: (
       <>
-        Leverage the power of Kubernetes native pipelines for seamless application management with the comprehensive EDP Library.
+        Leverage the power of Kubernetes native pipelines for seamless
+        application management with the comprehensive EDP Library.
       </>
     ),
     link: "docs/user-guide/application/",
   },
   {
-    title: 'GitOps',
-    Svg: require('@site/static/img/logo.svg').default,
+    title: "GitOps",
+    Svg: require("@site/static/img/logo.svg").default,
     description: (
       <>
-        Implement the GitOps approach for efficient deployment of your code and infrastructure. Maintain your state in code and manage changes effortlessly with our platform.
+        Implement the GitOps approach for efficient deployment of your code and
+        infrastructure. Maintain your state in code and manage changes
+        effortlessly with our platform.
       </>
     ),
     link: "docs/user-guide/add-cd-pipeline/",
   },
   {
-    title: 'Single sign-on',
-    Svg: require('@site/static/img/logo.svg').default,
+    title: "Single sign-on",
+    Svg: require("@site/static/img/logo.svg").default,
     description: (
       <>
-        Seamlessly integrate with your Enterprise Single Sign-On for a smooth and efficient user onboarding experience. Our platform is designed to enhance user accessibility and security, promoting a user-friendly environment that aligns with your enterprise’s unique needs.
+        Seamlessly integrate with your Enterprise Single Sign-On for a smooth
+        and efficient user onboarding experience. Our platform is designed to
+        enhance user accessibility and security, promoting a user-friendly
+        environment that aligns with your enterprise’s unique needs.
       </>
     ),
     link: "docs/operator-guide/install-keycloak/",
   },
   {
-    title: 'Optimized for Kubernetes',
-    Svg: require('@site/static/img/logo.svg').default,
+    title: "Optimized for Kubernetes",
+    Svg: require("@site/static/img/logo.svg").default,
     description: (
       <>
-        Experience the power of Kubernetes-native functionality. Our platform is designed to run seamlessly in your preferred Kubernetes or OpenShift clusters, offering you flexibility and control.
+        Experience the power of Kubernetes-native functionality. Our platform is
+        designed to run seamlessly in your preferred Kubernetes or OpenShift
+        clusters, offering you flexibility and control.
       </>
     ),
     link: "docs/operator-guide/install-kuberocketci/",
   },
   {
-    title: 'Advanced Security Analysis',
-    Svg: require('@site/static/img/logo.svg').default,
+    title: "Advanced Security Analysis",
+    Svg: require("@site/static/img/logo.svg").default,
     description: (
       <>
-        Achieve excellence in DevSecOps with our platform. We prioritize security, making it a mandatory quality gate for all operations. Experience enhanced protection and peace of mind.
+        Achieve excellence in DevSecOps with our platform. We prioritize
+        security, making it a mandatory quality gate for all operations.
+        Experience enhanced protection and peace of mind.
       </>
     ),
     link: "docs/operator-guide/devsecops/overview/",
   },
   {
-    title: 'Automated Test Analysis',
-    Svg: require('@site/static/img/logo.svg').default,
+    title: "Automated Test Analysis",
+    Svg: require("@site/static/img/logo.svg").default,
     description: (
       <>
-        Streamline your operations with our automated test analysis. Ensure your regressions pass seamlessly and predictably, enhancing the reliability of your systems. Leverage automation to increase your delivery speed and efficiency.
+        Streamline your operations with our automated test analysis. Ensure your
+        regressions pass seamlessly and predictably, enhancing the reliability
+        of your systems. Leverage automation to increase your delivery speed and
+        efficiency.
       </>
     ),
     link: "docs/operator-guide/advanced-install/reportportal/",
   },
   {
-    title: 'Comprehensive Metrics',
-    Svg: require('@site/static/img/logo.svg').default,
+    title: "Comprehensive Metrics",
+    Svg: require("@site/static/img/logo.svg").default,
     description: (
       <>
-        Gain access to your application logs and metrics with ease. Explore your application in real-time, gaining valuable insights and enhancing your understanding of its performance.
+        Gain access to your application logs and metrics with ease. Explore your
+        application in real-time, gaining valuable insights and enhancing your
+        understanding of its performance.
       </>
     ),
     link: "docs/operator-guide/monitoring-and-observability/multitenant-logging/",
   },
   {
-    title: 'Open-Source Software Solutions',
-    Svg: require('@site/static/img/logo.svg').default,
+    title: "Open-Source Software Solutions",
+    Svg: require("@site/static/img/logo.svg").default,
     description: (
       <>
-        Experience the power of open-source with EDP. Licensed under Apache 2.0, we equip you with leading open-source tools designed to enhance the reliability of your delivery process.
+        Experience the power of open-source with EDP. Licensed under Apache 2.0,
+        we equip you with leading open-source tools designed to enhance the
+        reliability of your delivery process.
       </>
     ),
     link: "docs/about-platform",
   },
 ];
 
-function Feature({title, Svg, description, link}: FeatureItem) {
+function Feature({ title, Svg, description, link }: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--left">
         <a href={link}>
-          <Svg className={styles.featureSvg} role="img" />
-          <Heading as="h3">{title}</Heading>
+          <div className={styles.grid}>
+            <Svg className={styles.featureSvg} role="img" />
+            <Heading as="h3" className={styles.title}>
+              {title}
+            </Heading>
+          </div>
           <p>{description}</p>
         </a>
       </div>
--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -1,21 +1,291 @@
-import React from 'react';
-import Layout from '@theme/Layout';
+import React from "react";
+import Layout from "@theme/Layout";
+import CheckIcon from "@mui/icons-material/Check";
+import "./pricing.css";
 
 export default function Hello() {
   return (
     <Layout title="Hello" description="Pricing">
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '50vh',
-          fontSize: '20px',
-        }}>
-        <p>
-          Edit <code>pages/pricing.tsx</code> and save to reload.
-        </p>
-      </div>
+      <section className="section section--pricing gradient-to-bottom">
+        <div className="md-grid">
+          <div className="section__content">
+            <h1 className="h1">Pricing</h1>
+            <p className="page-description">
+              Experience the flexibility of deploying the EPAM Delivery Platform
+              on your on-premises infrastructure or in the Cloud. Your valuable
+              data remains securely within your perimeter, ensuring
+              uncompromising security at every step.
+            </p>
+
+            <div className="pricing-grid">
+              <div className="pricing-grid__item">
+                <div className="pricing-card">
+                  <div className="pricing-card__content">
+                    <div className="pricing-card__name">Open Source</div>
+                    <div className="pricing-card__price">Free</div>
+                    <div className="pricing-card__description">
+                      The essentials for when you're just getting started.
+                    </div>
+                  </div>
+                  <div className="pricing-card__footer">
+                    <a href="/edp-install/getting-started" className="button">
+                      start
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="pricing-grid__item">
+                <div className="pricing-card">
+                  <div className="pricing-card__content">
+                    <div className="pricing-card__name">Team</div>
+                    <div className="pricing-card__price">
+                      $3,000
+                      <span>/month</span>
+                    </div>
+                    <div className="pricing-card__description">
+                      The essentials for when you're just getting started.
+                    </div>
+                    <ul className="pricing-card__features">
+                      <li className="pricing-card__feature">
+                        <span className="twemoji pricing-card__feature-marker">
+                          <CheckIcon />
+                        </span>
+                        25 professional service hours <sup>*</sup>
+                      </li>
+                      <li className="pricing-card__feature">
+                        <span className="twemoji pricing-card__feature-marker">
+                          <CheckIcon />
+                        </span>
+                        6 month minimum commitment <sup>**</sup>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="pricing-card__footer">
+                    <a
+                      href="mailto:SupportEPMD-EDP@epam.com"
+                      className="button"
+                    >
+                      contact us
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="pricing-grid__item">
+                <div className="pricing-card">
+                  <div className="pricing-card__content">
+                    <div className="pricing-card__name">Stream</div>
+                    <div className="pricing-card__price">
+                      $6,000
+                      <span>/month</span>
+                    </div>
+                    <div className="pricing-card__description">
+                      The essentials for when you're just getting started.
+                    </div>
+                    <ul className="pricing-card__features">
+                      <li className="pricing-card__feature">
+                        <span className="twemoji pricing-card__feature-marker">
+                          <CheckIcon />
+                        </span>
+                        60 professional service hours <sup>*</sup>
+                      </li>
+                      <li className="pricing-card__feature">
+                        <span className="twemoji pricing-card__feature-marker">
+                          <CheckIcon />
+                        </span>
+                        6 month minimum commitment <sup>**</sup>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="pricing-card__footer">
+                    <a
+                      href="mailto:SupportEPMD-EDP@epam.com"
+                      className="button"
+                    >
+                      contact us
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="pricing-grid__item">
+                <div className="pricing-card">
+                  <div className="pricing-card__content">
+                    <div className="pricing-card__name">Enterprise</div>
+                    <div className="pricing-card__price">
+                      $13,000
+                      <span>/month</span>
+                    </div>
+                    <div className="pricing-card__description">
+                      The essentials for when you're just getting started.
+                    </div>
+                    <ul className="pricing-card__features">
+                      <li className="pricing-card__feature">
+                        <span className="twemoji pricing-card__feature-marker">
+                          <CheckIcon />
+                        </span>
+                        160 professional service hours <sup>*</sup>
+                      </li>
+                      <li className="pricing-card__feature">
+                        <span className="twemoji pricing-card__feature-marker">
+                          <CheckIcon />
+                        </span>
+                        3 month minimum commitment <sup>**</sup>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="pricing-card__footer">
+                    <a
+                      href="mailto:SupportEPMD-EDP@epam.com"
+                      className="button"
+                    >
+                      contact us
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="table-wrapper">
+              <table className="table table--services">
+                <thead>
+                  <tr>
+                    <th>Service Category</th>
+                    <th>Description</th>
+                    <th>Professional Service Hours*</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td rowSpan={7}>
+                      <div className="table__cell-label">
+                        Deployment, Installation, Configuration
+                      </div>
+                    </td>
+                    <td>
+                      Deploy Platform with Minimum Components in the existing
+                      Kubernetes Cluster: CI/CD, integration with existing
+                      SonarQube, Repository Manager, Container Registry
+                    </td>
+                    <td>60</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      Deploy Platform in an empty AWS Account, including
+                      components: Infrastructure, CI/CD, SSO integration,
+                      SonarQube, DependencyTrack, Nexus Repository, DefectDojo,
+                      Logging, Monitoring, Docker registry, Observability
+                    </td>
+                    <td>8</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      Traceability and Observability customization: Storage
+                      backend configuration, performance tunning
+                    </td>
+                    <td>40</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      Monitoring stack customization: Custom Dashboards, Alerts,
+                      Notifications
+                    </td>
+                    <td>40</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      Logging stack customization: Custom filters and processing
+                      for fluent-bit, logs rotation policy tunning
+                    </td>
+                    <td>40</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      SonarQube Quality Gate and Quality Profile configuration
+                    </td>
+                    <td>8</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      Security (SAST/SCA) report analysis per single codebase
+                    </td>
+                    <td>8</td>
+                  </tr>
+                  <tr>
+                    <td rowSpan={2}>
+                      <div className="table__cell-label">
+                        Framework/Language
+                      </div>
+                    </td>
+                    <td>
+                      Onboard new Framework or Build tool with CI Pipelines:
+                      Implement EDP standard CI Pipeline steps (build, test,
+                      lint, push)
+                    </td>
+                    <td>40</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      Marketplace Template Development: Create new Template,
+                      Implement EDP standard CI Pipeline steps (build, test,
+                      lint, push)
+                    </td>
+                    <td>40</td>
+                  </tr>
+                  <tr>
+                    <td rowSpan={3}>
+                      <div className="table__cell-label">
+                        Custom Tool Integration
+                      </div>
+                    </td>
+                    <td>Integrate custom tool with CI Pipeline over API</td>
+                    <td>30</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      Integrate custom tool with EDP Portal UI over Kubernetes
+                      API
+                    </td>
+                    <td>40</td>
+                  </tr>
+                  <tr>
+                    <td>Integrate custom tool with EDP Portal UI over API</td>
+                    <td>80</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <div className="table__cell-label">
+                        Target Deployment Platform customization
+                      </div>
+                    </td>
+                    <td>
+                      Add Custom Deployment Platform: Implement custom CD
+                      Pipeline
+                    </td>
+                    <td>80</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+            <p className="definition">
+              <sup>*</sup>
+              Professional service hours are versatile blended hours bundled
+              within your subscription plan, encompassing expertise from diverse
+              specialists such as business analysts, architects, lead automation
+              engineers, DevOps (System Engineers), and performance engineers.
+              These multi-functional hours cater to EDP installation,
+              configuration, integration, customization, feature implementation,
+              updates, and use case implementation. They empower you to leverage
+              a range of expertise for various aspects of optimizing and
+              enhancing your EDP experience.
+            </p>
+            <p className="definition">
+              <sup>**</sup>
+              Minimum commitment of time for use of a certain subscription
+              package.
+            </p>
+          </div>
+        </div>
+      </section>
     </Layout>
   );
 }

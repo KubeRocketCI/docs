bfbe9dd2711a8d6429738bfff59f737ab04c57da
Sergiy Kulanov <sergiy_kulanov@epam.com>
2024-10-10 15:20:26 +0300

    Deploy website - based on 54dc1c5f9ee8ab28d3f771883f0f32a420bf473b

---
+++ assets/js/4767f0a4.39062681.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[2168],{85420:s=>{s.exports=JSON.parse('{"tag":{"label":"SSO","permalink":"/blog/tags/sso","allTagsPath":"/blog/tags","count":1,"unlisted":false},"listMetadata":{"permalink":"/blog/tags/sso","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/814f3328.272da66f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[7472],{55513:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Advanced AWS EKS Management: Implementing SSO via OIDC and Keycloak","permalink":"/blog/advanced-aws-eks-management-oidc-keycloak","unlisted":false,"date":"2024-10-04T00:00:00.000Z"}]}')}}]);

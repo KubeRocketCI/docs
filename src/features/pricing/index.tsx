7faec0ce519e9b299be2ff1a5ab3e506f9f66289
Vladyslav Palyvoda <vladyslav_palyvoda@epam.com>
2024-10-18 15:51:27 +0300

    docs: Update asterisks, fix npm scripts, adjust prettier cfg

--- src/features/pricing/index.tsx
+++ src/features/pricing/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import { PricingCard } from './components/Card';
-import { pricingList } from './constants';
+import { pricingList } from './list';
 import styles from './styles.module.css';
 import { Note } from '../../components/Note';
 import { FeaturesTable } from './components/FeaturesTable';
@@ -13,9 +13,8 @@ export const Pricing = () => {
         <div className="container">
           <h1 className={styles.sectionTitle}>Professional Service Pricing</h1>
           <div className={styles.sectionDescription}>
-            Experience the flexibility of deploying KubeRocketCI on your on-premises infrastructure or in
-            the Cloud. Your valuable data stays securely within your perimeter, ensuring uncompromising security at
-            every step.
+            Experience the flexibility of deploying KubeRocketCI on your on-premises infrastructure or in the Cloud.
+            Your valuable data stays securely within your perimeter, ensuring uncompromising security at every step.
           </div>
 
           <div className={styles.pricingGrid}>
@@ -27,15 +26,15 @@ export const Pricing = () => {
           </div>
 
           <Note>
-            Professional service hours are versatile, blended hours bundled within your subscription plan.
-            They encompass expertise from diverse specialists, including business analysts, architects, lead automation engineers, DevOps (System Engineers), and performance engineers.
-            These multi-functional hours cater to KubeRocketCI installation, configuration, integration, customization, feature implementation, updates, and use case implementation.
-            They empower you to leverage a range of expertise for various aspects of optimizing and enhancing your KubeRocketCI experience.
+            Professional service hours are versatile, blended hours bundled within your subscription plan. They
+            encompass expertise from diverse specialists, including business analysts, architects, lead automation
+            engineers, DevOps (System Engineers), and performance engineers. These multi-functional hours cater to
+            KubeRocketCI installation, configuration, integration, customization, feature implementation, updates, and
+            use case implementation. They empower you to leverage a range of expertise for various aspects of optimizing
+            and enhancing your KubeRocketCI experience.
           </Note>
 
-          <Note>
-            Minimum commitment of time for use of a certain subscription package.
-          </Note>
+          <Note asteriskAmount={2}>Minimum commitment of time for use of a certain subscription package.</Note>
 
           <FeaturesTable />
 

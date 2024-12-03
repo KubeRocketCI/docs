f03c8430c92880ae810843fefa6ec91a8d7698e5
Sergiy Kulanov <sergiy_kulanov@epam.com>
2024-12-03 17:23:06 +0200

    Deploy website - based on 1eef8d4c21fdb081b9e9e07b6f5bc24ccc8b9bf6

--- assets/js/4767f0a4.4a4543a6.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[72168],{85420:s=>{s.exports=JSON.parse('{"tag":{"label":"SSO","permalink":"/blog/tags/sso","allTagsPath":"/blog/tags","count":1,"unlisted":false},"listMetadata":{"permalink":"/blog/tags/sso","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);

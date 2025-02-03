ce0a654cdd8078bc4531f2ca231d0b2972960538
Vladyslav Palyvoda <vladyslav_palyvoda@epam.com>
2025-02-03 12:38:52 +0200

    feat: Add cookie pro consent widget

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -1,31 +1,23 @@
 import React from 'react';
-import CookieConsent from 'react-cookie-consent';
+import Head from '@docusaurus/Head';
 
 export default function Root({ children }) {
   return (
     <>
-      <CookieConsent
-        location="bottom"
-        buttonClasses="button button--secondary button--error margin-horiz--sm"
-        disableButtonStyles
-        buttonText="Accept"
-        setDeclineCookie
-        expires={150}
-        style={{
-          backgroundColor: 'var(--ifm-color-primary)',
-          alignItems: 'center',
-          padding: '1rem 2rem',
-          width: '80%',
-          left: '50%',
-          transform: 'translateX(-50%)',
-          borderRadius: '0.25rem',
-          marginBottom: '20px',
-        }}
-      >
-        We use cookies to recognize your repeated visits and preferences, as well as to measure the effectiveness of our
-        documentation and whether users find what they're searching for. With your consent, you're helping us to make
-        our documentation better.
-      </CookieConsent>
+      <link rel="stylesheet" href="/css/cookie-consent.css" />
+      <Head>
+        <script
+          type="text/javascript"
+          src="https://cookie-cdn.cookiepro.com/consent/0194a78c-7932-70b9-abdc-9d8096b5f261/OtAutoBlock.js"
+        ></script>
+        <script
+          src="https://cookie-cdn.cookiepro.com/scripttemplates/otSDKStub.js"
+          type="text/javascript"
+          charSet="UTF-8"
+          data-domain-script="0194a78c-7932-70b9-abdc-9d8096b5f261"
+        ></script>
+        <script type="text/javascript">{`function OptanonWrapper() {}`}</script>
+      </Head>
       {children}
     </>
   );
